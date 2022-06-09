cb175f50315c7ec15af42e55a452f53570104c23
Christian Deantana <christiandeantanastudy@gmail.com>
2022-06-09 23:58:19 +0800

    Create insertion_sort.py

---
+++ Algorithm/Sorting/insertion_sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(A):
+    n = len(A)
+    for i in range(1,n):
+        cvalue = A[i]
+        position = i
+        while position > 0 and A[position] > cvalue:
+            A[position] = A[position-1]
+            position = position - 1
+        A[position] = cvalue
+
+Array = [2,3,1,11,6,4]
+insertion_sort(Array)
+print(Array)
