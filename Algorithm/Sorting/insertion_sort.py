02c204bdd1c381e3d11aa555391fbac73e17ee71
Christian Deantana <christiandeantanastudy@gmail.com>
2022-06-09 23:58:54 +0800

    Update insertion_sort.py

--- Algorithm/Sorting/insertion_sort.py
+++ Algorithm/Sorting/insertion_sort.py
@@ -3,7 +3,7 @@ def insertion_sort(A):
     for i in range(1,n):
         cvalue = A[i]
         position = i
-        while position > 0 and A[position] > cvalue:
+        while position > 0 and A[position-1] > cvalue:
             A[position] = A[position-1]
             position = position - 1
         A[position] = cvalue
